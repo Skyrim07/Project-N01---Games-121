a874a88b7306ca63844598b607f55dd4f145ed0b
Alex Liu <63307936+Skyrim07@users.noreply.github.com>
2022-01-31 12:08:37 -0500

    Initial commit

---
+++ Assets/Switch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class Switch : MonoBehaviour
+{
+    public GameObject g1, g2;
+
+    public void G1()
+    {
+        g1.SetActive(true);
+        g2.SetActive(false);
+    }
+
+    public void G2()
+    {
+        g1.SetActive(false);
+        g2.SetActive(true);
+    }
+}
---
+++ Assets/get.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public sealed class get : MonoBehaviour
+{
+    public GameObject g;
+    private void OnEnable()
+    {
+        if (!g)
+            return;
+
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject a = Instantiate(g);
+            a.GetComponent<SpriteRenderer>().color = new Color(1, .9f, .7f, Random.Range(.1f, .9f));
+        }
+    }
+}
